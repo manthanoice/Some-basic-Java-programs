6b0fe2aca9d8e6b3135f3ca8afa1f80f1f3de1db
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 23:08:18 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Sum {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the two numbers you want to do sum of: ");
+        System.out.println("Enter the first number: ");
         int x = sc.nextInt();
+        System.out.println("Enter the second number: ");
         int y = sc.nextInt();
         int sum = x+y;
         System.out.println("The sum of " + x + "+" + y + " = "+sum);
