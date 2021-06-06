caf1c561830c8505fabb67561e59673811ee2475
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 23:01:53 +0530

    Update ASCII.java

--- ASCII.java
+++ ASCII.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ASCII {
     
-    public static void main (String args[]){
+    public static void main (String[] args){
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the charter whose ASCII value you want to find: ");
