b43a853217a4cb4ca9cc78894ade1898286e9dbd
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 23:03:15 +0530

    Update FloatMultiply.java

--- FloatMultiply.java
+++ FloatMultiply.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class FloatMultiply {
-    public static void main (String args[]){
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two floating numbers you want to multiply: ");
         float x = sc.nextFloat();
