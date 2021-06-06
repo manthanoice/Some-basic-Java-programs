5f490683955f28ddb4603175487fe3dc7993acca
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 23:03:50 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -13,7 +13,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Swap {
-    public static void main (String args[]){
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the first number: ");
         int x = sc.nextInt();
