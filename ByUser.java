ea6399447b12bd98218918b2d94005a9fd276d52
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 16:50:18 +0530

    Update ByUser.java

--- ByUser.java
+++ ByUser.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class ByUser {
     public static void main (String args[]){
+        
         Scanner sc = new Scanner(System.in); //To take input from keyboard by the user
+        
         System.out.println("Enter the number you want to enter: ");
         int x = sc.nextInt(); //To read the input entered by user
         System.out.println("The number you have entered is: "+x);
