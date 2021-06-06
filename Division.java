2f89a40af9a1bf8e010e23600f48821b9aac9336
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 23:02:48 +0530

    Update Division.java

--- Division.java
+++ Division.java
@@ -15,7 +15,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Division {
-    public static void main (String args[]){
+    public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter dividend: ");
         int dividend = sc.nextInt();
