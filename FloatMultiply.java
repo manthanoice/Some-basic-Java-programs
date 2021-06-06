a765811ca75c0d73e9f058824ebcc64694cfe8c8
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-06-06 23:09:52 +0530

    Update FloatMultiply.java

--- FloatMultiply.java
+++ FloatMultiply.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class FloatMultiply {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter two floating numbers you want to multiply: ");
+        System.out.println("Enter first floating number: ");
         float x = sc.nextFloat();
+        System.out.println("Enter second floating number: ");
         float y = sc.nextFloat();
         float sum = x*y;
-        System.out.println("The sum of " + x + "*" + y + " = " + sum);
+        System.out.println("The sum of " + x + " * " + y + " = " + sum);
     }
 }
