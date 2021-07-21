31f223852bc7eda72311f1c1bd563cd173552518
manthanoice <69193208+manthanoice@users.noreply.github.com>
2021-07-21 23:25:43 +0530

    Update Main.java

--- src/LinkedList2/Main.java
+++ src/LinkedList2/Main.java
@@ -1,3 +1,18 @@
+/*
+    OUTPUT:
+    count is: 8
+    YES
+    At index n is: 
+    9299
+    Data is: 
+    2222 234 9299 34 9939 99 293 99 
+    From last data is: 
+    293
+    Middle is: 
+    9939
+    Count is: 2
+*/
+
 package LinkedList2;
 
 public class Main {
@@ -8,11 +23,19 @@ public class Main {
         l.append(99);
         l.indexing(1,9939);
         l.deleting(2);
+        l.push(9299);
+        l.push(234);
+        l.append(293);
+        l.append(99);
+        l.push(2222);
         l.counting();
         String s = (l.search(9939)?"YES":"NO");
         System.out.println(s);
         l.getData(2);
 //        l.deleteList();
         l.printing();
+        l.fromLast(1);
+        l.middle();
+        l.countElement(99);
     }
 }
